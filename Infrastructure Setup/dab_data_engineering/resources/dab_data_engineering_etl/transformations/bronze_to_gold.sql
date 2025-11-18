1369fcc9c0936e7808b2959225293b0badb24b48
robin_huebner@hotmail.com <robin_huebner@hotmail.com>
2025-11-18 09:59:22 +0000

    Changed workspace to Vending Machine

--- Infrastructure Setup/dab_data_engineering/resources/dab_data_engineering_etl/transformations/bronze_to_gold.sql
+++ Infrastructure Setup/dab_data_engineering/resources/dab_data_engineering_etl/transformations/bronze_to_gold.sql
@@ -1,69 +1,67 @@
--- Create a materialized view for the date dimension from CSV files
-CREATE VIEW dim_date AS
+-- Date dimension (CSV -> bronze streaming)
+CREATE OR REFRESH STREAMING TABLE bronze.date AS
 SELECT
     *
-FROM read_files(
-  "/Volumes/sunny_bay_roastery/bronze/raw/dim_date/",
-  format => "csv"
+FROM STREAM read_files(
+  '/Volumes/sunny_bay_roastery/bronze/raw/dim_date/',
+  format => 'csv'
 );
 
--- Create a view for the store dimension from CSV files
-CREATE VIEW dim_store AS
+-- Store dimension (CSV -> bronze streaming)
+CREATE OR REFRESH STREAMING TABLE bronze.store AS
 SELECT
     *
-FROM read_files(
-  "/Volumes/sunny_bay_roastery/bronze/raw/dim_store/",
-  format => "csv"
+FROM STREAM read_files(
+  '/Volumes/sunny_bay_roastery/bronze/raw/dim_store/',
+  format => 'csv'
 );
 
--- Create a view for the customer dimension from CSV files
-CREATE VIEW dim_customer AS
+-- Customer dimension (CSV -> bronze streaming)
+CREATE OR REFRESH STREAMING TABLE bronze.customer AS
 SELECT
     *
-FROM read_files(
-  "/Volumes/sunny_bay_roastery/bronze/raw/dim_customer/",
-  format => "csv"
+FROM STREAM read_files(
+  '/Volumes/sunny_bay_roastery/bronze/raw/dim_customer/',
+  format => 'csv'
 );
 
--- Create a view for the product dimension from CSV files
-CREATE VIEW dim_product AS
+-- Product dimension (CSV -> bronze streaming)
+CREATE OR REFRESH STREAMING TABLE bronze.product AS
 SELECT
     *
-FROM read_files(
-  "/Volumes/sunny_bay_roastery/bronze/raw/dim_product/",
-  format => "csv"
+FROM STREAM read_files(
+  '/Volumes/sunny_bay_roastery/bronze/raw/dim_product/',
+  format => 'csv'
 );
 
--- Create a view for the coffee sales fact table from Parquet files
-CREATE VIEW fact_coffee_sales AS
+-- Coffee sales fact (Parquet -> bronze streaming)
+CREATE OR REFRESH STREAMING TABLE bronze.coffee_sales AS
 SELECT
     *
-FROM read_files(
-  "/Volumes/sunny_bay_roastery/bronze/raw/fact_coffee_sales/",
-  format => "parquet"
+FROM STREAM read_files(
+  '/Volumes/sunny_bay_roastery/bronze/raw/fact_coffee_sales/',
+  format => 'parquet'
 );
 
+CREATE MATERIALIZED VIEW gold.dim_date AS
+SELECT * FROM bronze.date;
 
---------------------------------------
-CREATE MATERIALIZED VIEW sunny_bay_roastery.gold.dim_date AS
-SELECT * FROM dim_date;
+CREATE MATERIALIZED VIEW gold.dim_store AS
+SELECT * FROM bronze.store;
 
-CREATE MATERIALIZED VIEW sunny_bay_roastery.gold.dim_store AS
-SELECT * FROM dim_store ds;
+CREATE MATERIALIZED VIEW gold.dim_customer AS
+SELECT * FROM bronze.customer;
 
-CREATE MATERIALIZED VIEW sunny_bay_roastery.gold.dim_customer AS
-SELECT * FROM dim_customer;
+CREATE MATERIALIZED VIEW gold.dim_product AS
+SELECT * FROM bronze.product;
 
-CREATE MATERIALIZED VIEW sunny_bay_roastery.gold.dim_product AS
-SELECT * FROM dim_product;
-
-CREATE MATERIALIZED VIEW sunny_bay_roastery.gold.fact_coffee_sales AS
+CREATE MATERIALIZED VIEW gold.fact_coffee_sales AS
 SELECT
-    fcs.*,
-    dp.list_price_usd * fcs.quantity_sold AS gross_revenue_usd,
-    (dp.list_price_usd * fcs.quantity_sold) / (1 + ds.tax_rate) AS net_revenue_usd
-FROM fact_coffee_sales fcs
-JOIN dim_product dp
+    fcs.*
+    -- dp.list_price_usd * fcs.quantity_sold                               AS gross_revenue_usd,
+    -- (dp.list_price_usd * fcs.quantity_sold) / (1 + ds.tax_rate)         AS net_revenue_usd
+FROM bronze.coffee_sales fcs
+JOIN bronze.product dp
   ON fcs.product_key = dp.product_key
-JOIN dim_store ds
+JOIN bronze.store ds
   ON fcs.store_key = ds.store_key;
